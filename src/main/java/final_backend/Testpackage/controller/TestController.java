2542112cb2f7cff936a5fbf35c30c765725613e9
rhquddus <rhquddus@naver.com>
2023-07-14 01:10:07 +0900

    thymeleaf added

---
+++ src/main/java/final_backend/Testpackage/controller/TestController.java
@@ -0,0 +1,12 @@
+package final_backend.Testpackage.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+    @GetMapping
+    public String showIndex(){
+        return "index";
+    }
+}
