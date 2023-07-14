28ceed645e51282a17b7cb9cae187cd3a06bddfc
rhquddus <rhquddus@naver.com>
2023-07-14 17:44:16 +0900

    test page modified

--- src/main/java/final_backend/Testpackage/controller/TestController.java
+++ src/main/java/final_backend/Testpackage/controller/TestController.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TestController {
-    @GetMapping
+    @GetMapping("/test")
     public String showIndex(){
-        return "index";
+        return "test";
     }
 }
