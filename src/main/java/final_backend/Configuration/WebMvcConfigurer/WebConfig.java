63925825a99f6e0a8af33af65733edfeb8e04b35
rhquddus <rhquddus@naver.com>
2023-07-17 16:02:23 +0900

    add backend

--- src/main/java/final_backend/Configuration/WebMvcConfigurer/WebConfig.java
+++ src/main/java/final_backend/Configuration/WebMvcConfigurer/WebConfig.java
@@ -1,36 +1,19 @@
 package final_backend.Configuration.WebMvcConfigurer;
 
-import org.apache.catalina.filters.CorsFilter;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOrigin("http://localhost:3000"); // 프론트엔드 로컬서버로 테스트
-//        config.addExposedHeader(JwtUtil.AUTHORIZATION_HEADER);
-        config.addAllowedMethod("*");
-        config.addAllowedHeader("*");
-        config.setAllowCredentials(true);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
-
-        return source;
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://180.150.207.73:80")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("Authorization", "Content-Type")
+                .exposedHeaders("Custom-Header")
+                .allowCredentials(true)
+                .maxAge(3600);
     }
-//
-//    @Bean
-//    public CorsFilter corsFilter() {
-//        CorsConfigurationSource source = corsConfigurationSource();
-//        return new CorsFilter(source);
-//    }
-
 }
