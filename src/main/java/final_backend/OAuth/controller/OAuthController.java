253c783165b90a5fb123395acaf2650926681ffc
mastgm0817 <mastgm0817@gmail.com>
2023-07-24 01:53:22 +0900

    Social login v1.2 structue splited

--- src/main/java/final_backend/OAuth/controller/OAuthController.java
+++ src/main/java/final_backend/OAuth/controller/OAuthController.java
@@ -1,90 +1,32 @@
 package final_backend.OAuth.controller;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 import final_backend.OAuth.service.OAuthService;
-import org.json.JSONObject;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.*;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 
 @RestController
-//@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/oauth")
 public class OAuthController {
-
-    private static final Logger log = LoggerFactory.getLogger(OAuthController.class);
 
     @Autowired
     private OAuthService oauthService;
 
+
     @GetMapping("/kakao")
     public ResponseEntity<Map<String, Object>> KakaoLogin(@RequestParam("code") String code, HttpServletRequest request) {
-
-        RestTemplate rt = new RestTemplate();
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
-
-        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-        params.add("grant_type", "authorization_code");
-        params.add("client_id", "25edd438130f1d799655087b02557293");
-        params.add("redirect_uri", "http://localhost:3000/oauth/kakao");
-        params.add("code", code);
-        params.add("client_secret", "pcIl6N5qw8Y7s4OtUNZyVpep0xEuVmxf");
-
-        HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest = new HttpEntity<>(params, httpHeaders);
-
-        ResponseEntity<String> response = rt.exchange(
-                "https://kauth.kakao.com/oauth/token",
-                HttpMethod.POST,
-                kakaoTokenRequest,
-                String.class
-        );
-
-        // 토큰값 Json 형식으로 가져오기위해 생성
-        JSONObject jo = new JSONObject(response.getBody());
-
-        // 토큰결과값
-        log.debug("kakao token result = {} " , response);
-
-        RestTemplate rt2 = new RestTemplate();
-        HttpHeaders headers2 = new HttpHeaders();
-
-        headers2.add("Authorization", "Bearer " + jo.get("access_token"));
-//        headers2.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
-
-        HttpEntity<MultiValueMap<String, String>> kakaoProfileRequest2 = new HttpEntity<>(headers2);
-
-        ResponseEntity<String> response2 = rt2.exchange(
-                "https://kapi.kakao.com/v2/user/me",
-                HttpMethod.POST,
-                kakaoProfileRequest2,
-                String.class
-        );
-
-
-        // 토큰을 사용하여 사용자 정보 추출
-//        JSONObject jo2 = new JSONObject(response2.getBody());
-//        log.debug("###### kakao login = {}", jo2);
-//
-//        // 이후 유저 여부를 판단하고 회원가입 / 로그인 처리를 진행하면된다
-//
-//        return new ResponseEntity<>(jo2, HttpStatus.OK);
-
-        Map<String, Object> userMap = new Gson().fromJson(response2.getBody(), new TypeToken<Map<String, Object>>(){}.getType());
-        log.debug("###### kakao login = {}", userMap);
-
+        Map<String, Object> userMap = oauthService.getUserInfo(code);
+//        log.debug("###### kakao login = {}", userMap);
+//        oauthService.saveUser(userMap);
         // 이후 유저 여부를 판단하고 회원가입 / 로그인 처리를 진행하면된다
 
         return new ResponseEntity<>(userMap, HttpStatus.OK);
     }
-
 }
--- src/main/java/final_backend/OAuth/service/OAuthService.java
+++ src/main/java/final_backend/OAuth/service/OAuthService.java
@@ -1,37 +1,100 @@
 package final_backend.OAuth.service;
 
-import final_backend.OAuth.model.KakaoOAuthToken;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import final_backend.Member.repository.UserRepository;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.http.*;
-import org.springframework.util.*;
-import org.springframework.web.reactive.function.BodyInserters;
-import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
+
+import java.util.Map;
 
 @Service
 public class OAuthService {
-//    private final WebClient webClient;
-    private final String KAKAO_TOKEN_URL = "https://kauth.kakao.com/oauth/token";
-    private String REST_API_KEY = "25edd438130f1d799655087b02557293";
 
-//    public OAuthService(WebClient webClient) {
-//        this.webClient = webClient;
-//    }
+    @Autowired
+    private UserRepository userRepository;
+
+    private String client_id = System.getenv("CLIENT_ID");
+
+    private String client_secret = System.getenv("CLIENT_SECRET");
+
+//카카오 사용자 인증
+    public Map<String, Object> getUserInfo(String code) {
+        RestTemplate rt = new RestTemplate();
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", "authorization_code");
+        params.add("client_id", client_id);
+        params.add("redirect_uri", "http://localhost:3000/oauth/kakao");
+        params.add("code", code);
+        params.add("client_secret", client_secret);
+
+        HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest = new HttpEntity<>(params, httpHeaders);
+        System.out.println("------------헤더--------------");
+        System.out.println(kakaoTokenRequest.getHeaders());
+        System.out.println("------------본문--------------");
+        System.out.println(kakaoTokenRequest.getBody());
+
+
+        ResponseEntity<String> response = rt.exchange(
+                "https://kauth.kakao.com/oauth/token",
+                HttpMethod.POST,
+                kakaoTokenRequest,
+                String.class
+        );
+        System.out.println("------------response body--------------");
+        System.out.println(response.getBody());
+// TODO: 2023-07-24(024)
+
+
+//        앱사용자 인증 부분
+        JSONObject jo = new JSONObject(response.getBody());
+
+        RestTemplate rt2 = new RestTemplate();
+        HttpHeaders headers2 = new HttpHeaders();
+
+        headers2.add("Authorization", "Bearer " + jo.get("access_token"));
+
+        HttpEntity<MultiValueMap<String, String>> kakaoProfileRequest2 = new HttpEntity<>(headers2);
+
+        ResponseEntity<String> response2 = rt2.exchange(
+                "https://kapi.kakao.com/v2/user/me",
+                HttpMethod.POST,
+                kakaoProfileRequest2,
+                String.class
+        );
+        System.out.println("------------response2 body--------------");
+        System.out.println(response2.getBody());
+        Map<String, Object> userMap = new Gson().fromJson(response2.getBody(), new TypeToken<Map<String, Object>>(){}.getType());
+        return userMap;
+    }
+
+//    public User saveUser(Map<String, Object> userMap) {
 //
-//    public Mono<String> getKakaoToken(String code) {
-//        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-//        params.add("grant_type", "authorization_code");
-//        params.add("client_id", REST_API_KEY);
-//        params.add("redirect_uri", "http://localhost:3000/oauth/kakao");
-//        params.add("code", code);
+//        User user = userRepository.findByEmail();
 //
-//        return webClient.post()
-//                .uri(KAKAO_TOKEN_URL)
-//                .header(HttpHeaders.CONTENT_TYPE, "application/x-www-form-urlencoded;charset=utf-8")
-//                .body(BodyInserters.fromFormData(params))
-//                .retrieve()
-//                .bodyToMono(KakaoOAuthToken.class)
-//                .map(KakaoOAuthToken::getAccessToken);
+//        //(3)
+//        if (user == null) {
+//            user = User.builder()
+//                    .kakaoId(profile.getId())
+//                    //(4)
+//                    .kakaoProfileImg(profile.getKakao_account().getProfile().getProfile_image_url())
+//                    .kakaoNickname(profile.getKakao_account().getProfile().getNickname())
+//                    .kakaoEmail(profile.getKakao_account().getEmail())
+//                    //(5)
+//                    .userRole("ROLE_USER").build();
+//
+//            userRepository.save(user);
+//        }
 //    }
 }
