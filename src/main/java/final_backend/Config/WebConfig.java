cb236ad57328148809c9005255913dfa56bdba37
rhquddus <rhquddus@naver.com>
2023-08-04 04:27:32 +0900

    cors

--- src/main/java/final_backend/Config/WebConfig.java
+++ src/main/java/final_backend/Config/WebConfig.java
@@ -10,6 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://luvoost.co.kr")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
