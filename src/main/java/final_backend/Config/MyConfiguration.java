803189e74dfcaf53c9b43065010e3b887179af65
rhquddus <rhquddus@naver.com>
2023-08-04 15:28:03 +0900

    productio environment setting

---
+++ src/main/java/final_backend/Config/MyConfiguration.java
@@ -0,0 +1,24 @@
+package final_backend.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class MyConfiguration {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("**")
+//                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://luvoost.co.kr")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+            }
+        };
+    }
+}
