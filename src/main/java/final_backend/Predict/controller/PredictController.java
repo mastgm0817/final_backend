2d56fdfdbbafbacade278cd66fc33f98845f5922
KHB2937 <gmlqja7913@gmail.com>
2023-08-21 21:01:13 +0900

    csv 데이터 추가

--- src/main/java/final_backend/Predict/controller/PredictController.java
+++ src/main/java/final_backend/Predict/controller/PredictController.java
@@ -26,4 +26,20 @@ public class PredictController {
 //        String predictionResult = "{\"latitude\":" + latitude + ", \"longitude\":" + longitude + "}";
         return predictionResult;
     }
+
+    @PostMapping("/random")
+    public String random(@RequestBody PredictDTO predictDTO){
+        String flaskServerUrl = "http://127.0.0.1:5000/api/v1/random"; // 실제 Flask 서버의 URL로 변경해주세요
+        // Flask 서버로 데이터 전송
+        RestTemplate restTemplate = new RestTemplate();
+        System.out.println("Spring Connected And Try Flask Connection");
+        ResponseEntity<String> response = restTemplate.postForEntity(flaskServerUrl, predictDTO, String.class);
+        System.out.println("wow");
+        String predictionResult = response.getBody();
+        // 예측 결과를 JSON 형식으로 생성하여 반환합니다.
+//        String predictionResult = "{\"latitude\":" + latitude + ", \"longitude\":" + longitude + "}";
+        return predictionResult;
+    }
+
+
 }
