4f5faca4ec3bdd6fdfc172f58faa8ba8ae4cc2cf
mastgm0817 <mastgm0817@gmail.com>
2023-08-23 14:53:42 +0900

    backend cicd

--- src/main/java/final_backend/Config/MyConfiguration.java
+++ src/main/java/final_backend/Config/MyConfiguration.java
@@ -1,27 +1,27 @@
-package final_backend.Config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-
-@Configuration
-public class MyConfiguration {
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
-                        .allowedOrigins("https://luvoost.co.kr")
-                        .allowedOrigins("http://luvoost.co.kr")
-                        .allowedOrigins("https://180.150.207.73:32000")
-                        .allowedOrigins("http://180.150.207.73:32000")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE");
-            }
-        };
-    }
-}
+//package final_backend.Config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//
+//@Configuration
+//public class MyConfiguration {
+//
+//    @Bean
+//    public WebMvcConfigurer corsConfigurer() {
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/**")
+//                        .allowedOrigins("http://localhost:3000")
+//                        .allowedOrigins("https://luvoost.co.kr")
+//                        .allowedOrigins("http://luvoost.co.kr")
+//                        .allowedOrigins("https://180.150.207.73:32000")
+//                        .allowedOrigins("http://180.150.207.73:32000")
+//                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+//            }
+//        };
+//    }
+//}
