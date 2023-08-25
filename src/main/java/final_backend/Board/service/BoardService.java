57b3985b1215d9760654a1b62bbf17da5bf5a936
whataLIN <kitschh@naver.com>
2023-08-25 17:06:58 +0900

    Pagenation bug fix

--- src/main/java/final_backend/Board/service/BoardService.java
+++ src/main/java/final_backend/Board/service/BoardService.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface BoardService {
-    List<Board> getAllBoards();
+    Long getAllBoards();
 
     Optional<Board> getBoardById(Long bid);
 
