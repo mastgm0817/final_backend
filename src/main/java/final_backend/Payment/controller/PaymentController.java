822fb9b619907519a0521735b918b6e78ac56822
mastgm0817 <mastgm0817@gmail.com>
2023-08-29 13:55:13 +0900

    카카오페이 구축중

--- src/main/java/final_backend/Payment/controller/PaymentController.java
+++ src/main/java/final_backend/Payment/controller/PaymentController.java
@@ -1,24 +1,38 @@
 package final_backend.Payment.controller;
 
+import final_backend.Payment.model.KakaoApproveResponse;
+import final_backend.Payment.model.KakaoReadyResponse;
 import final_backend.Payment.service.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/v1/payment")
 public class PaymentController {
 
     @Autowired
     private PaymentService paymentService;
 
-    @PostMapping("/kakao")
-    public String requestKakaoPay(@RequestBody Map<String, Object> payinfo) {
-        return paymentService.requestKakaoPay(payinfo);
+    @PostMapping("/ready")
+    public KakaoReadyResponse readyToKakaoPay() {
+
+        return paymentService.kakaoPayReady();
+    }
+
+    /**
+     * 결제 성공
+     */
+    @GetMapping("/success")
+    public ResponseEntity afterPayRequest(@RequestParam("pg_token") String pgToken) {
+
+        KakaoApproveResponse kakaoApprove = paymentService.ApproveResponse(pgToken);
+
+        return new ResponseEntity<>(kakaoApprove, HttpStatus.OK);
     }
 
 
